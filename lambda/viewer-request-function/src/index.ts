31394917901889390915805ece8726e18f35a3be
Simon Schoof <simon.schoof@hey.com>
2022-06-26 16:47:32 +0200

    #49 | simplify viewer request code

--- lambda/viewer-request-function/src/index.ts
+++ lambda/viewer-request-function/src/index.ts
@@ -1,13 +1,13 @@
 import { CloudFrontRequest } from "aws-lambda";
 
 interface ResizeParameters {
-    width?: number;
-    height?: number;
+    width: number;
+    height: number;
 }
 
 const AllowedDimensions = {
-    maxWidth: 1000,
-    maxHeight: 1000,
+    maxWidth: 10000,
+    maxHeight: 10000,
 }
 
 export async function handler(event: { Records: { cf: { request: any; } }[]; }): Promise<CloudFrontRequest> {
@@ -19,13 +19,9 @@ export async function handler(event: { Records: { cf: { request: any; } }[]; }):
 
     const params = parseParams(urlsSearchParams);
 
-    if (!validateParams(params)) {
+    if (paramsValid(params)) {
         console.log("Provided dimensions: width: " + params.width + " height: " + params.height);
         console.log("Request querystring: ", request.querystring);
-
-        request.querystring = `width=${params.width}&height=${params.height}`;
-        console.log("New request querystring: ", request.querystring);
-
     } else {
         console.log("No dimension or invalid dimension params found, returning original image");
         request.querystring = "";
@@ -39,18 +35,8 @@ function parseParams(params: URLSearchParams): ResizeParameters {
     const widthString = params.get('width');
     const heightString = params.get('height');
 
-    if (widthString === null || heightString === null) {
-        const resizerParams: ResizeParameters = {
-            width: undefined,
-            height: undefined,
-        }
-        return resizerParams
-    }
-
-    const width: number = (parseInt(widthString, 10) || AllowedDimensions.maxWidth) > AllowedDimensions.maxWidth ?
-        AllowedDimensions.maxWidth : parseInt(widthString, 10);
-    const height: number = (parseInt(heightString, 10) || AllowedDimensions.maxHeight) > AllowedDimensions.maxHeight ?
-        AllowedDimensions.maxHeight : parseInt(heightString, 10);
+    const width: number = widthString ? parseInt(widthString, 10) : NaN;
+    const height: number = heightString ? parseInt(heightString, 10): NaN; 
 
     const resizerParams: ResizeParameters = {
         width: width,
@@ -60,6 +46,11 @@ function parseParams(params: URLSearchParams): ResizeParameters {
 
 }
 
-function validateParams(params: ResizeParameters) {
-    return !params.width || !params.height || params.width <= 0 || params.height <= 0;
+function paramsValid(params: ResizeParameters) {
+    return !isNaN(params.width) 
+    && !isNaN(params.height)
+    && params.width > 0 
+    && params.height > 0
+    && params.width <= AllowedDimensions.maxWidth
+    && params.height <= AllowedDimensions.maxHeight;
 }
