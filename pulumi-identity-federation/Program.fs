7c0be8ad0f912bcc31e41987c4ef07a676bc9bfb
Simon Schoof <simon.schoof@hey.com>
2022-07-31 20:06:07 +0200

    #52 | add policy to allow OAI creation

--- pulumi-identity-federation/Program.fs
+++ pulumi-identity-federation/Program.fs
@@ -1,12 +1,36 @@
 ﻿module Program
 
 open Pulumi.FSharp
-open Pulumi.Aws.S3
 open Pulumi.Aws.Iam
 open Pulumi.Aws.Iam.Inputs
 
 let infra () =
 
+  let cloudFrontPolicy =
+
+        let cloudFrontStatement =
+            GetPolicyDocumentStatementInputArgs(
+                Actions = inputList [ input "cloudfront:CreateCloudFrontOriginAccessIdentity" ],
+                Resources =
+                    inputList [ input "*" ]
+            )
+
+
+        let policyDocumentInvokeArgs =
+            GetPolicyDocumentInvokeArgs(
+                Statements =
+                    inputList [ input cloudFrontStatement ]
+            )
+
+        let policyDocument =
+            GetPolicyDocument.Invoke(policyDocumentInvokeArgs)
+
+        let policyArgs =
+            PolicyArgs(PolicyDocument = io (policyDocument.Apply(fun (pd) -> pd.Json)))
+
+        Policy("cloudFrontPolicy", policyArgs)
+  
+  
   let openIdConnectProviderArgs = OpenIdConnectProviderArgs(
       Url = "https://token.actions.githubusercontent.com",
       ClientIdLists = inputList [input "sts.amazonaws.com"],
@@ -14,11 +38,11 @@ let infra () =
 
   let openIdConnectProvider = OpenIdConnectProvider("GithubOidc", openIdConnectProviderArgs)
 
-  let githubIamRole =
+  let federatedPrincipal =
+    GetPolicyDocumentStatementPrincipalInputArgs(Type = "Federated", Identifiers = inputList [ io openIdConnectProvider.Arn])
+  
+  let githubActionsRole =
     
-    let federatedPrincipal =
-        GetPolicyDocumentStatementPrincipalInputArgs(Type = "Federated", Identifiers = inputList [ io openIdConnectProvider.Arn])
-
     let assumeRoleWithWebIdentityStatement =
         GetPolicyDocumentStatementInputArgs(
             Principals =
@@ -42,11 +66,14 @@ let infra () =
         GetPolicyDocument.Invoke(policyDocumentInvokeArgs)
 
     Role("githubActionsRole",
-      RoleArgs(Name= "githubActionsRole", AssumeRolePolicy = io (policyDocument.Apply(fun (pd) -> pd.Json)))
+      RoleArgs(
+        Name= "githubActionsRole", 
+        AssumeRolePolicy = io (policyDocument.Apply(fun (pd) -> pd.Json)),
+        ManagedPolicyArns = inputList [ io cloudFrontPolicy.Arn])
         )
 
   dict [("openIdConnectProvider", openIdConnectProvider.Id :> obj);
-        ("githubIamRole", githubIamRole.Id :> obj)]
+        ("githubIamRole", githubActionsRole.Id :> obj)]
 
 [<EntryPoint>]
 let main _ =
